f8e356232b58e32b45a9f692b213849d7a0e8f5b
Van Dung Phan <62056772+dungphan294@users.noreply.github.com>
2025-06-11 08:37:57 +0200

    https config

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from backend.database import Base, engine  # adjust import if needed
 from backend.routers import user, phone, device, workout, data, user_device, user_phone  # under backend/api/from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+import ssl
 # from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 # Ensure the tables are created before the app starts
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain('../../cert.pem', keyfile='../../key.pem')
+
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 # app.add_middleware(HTTPSRedirectMiddleware)
 # Include routes
